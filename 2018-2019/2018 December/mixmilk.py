9d371ebb8d8077d7ea6b50b5d1af0ff9451c9858
tsx358 <97471993+tsx358@users.noreply.github.com>
2022-09-17 22:15:26 -0400

    Update mixmilk.py

--- 2018-2019/2018 December/mixmilk.py
+++ 2018-2019/2018 December/mixmilk.py
@@ -2,51 +2,30 @@ import sys
 sys.stdin = open("mixmilk.in","r")
 sys.stdout = open("mixmilk.out","w")
 
-c1,m1 = map(int, input().split()) #input capacities and current milk
+c1,m1 = map(int, input().split())
 c2,m2 = map(int, input().split())
 c3,m3 = map(int, input().split())
 
-milks = [m1,m2,m3] #milk amounts
-cs = [c1,c2,c3] #capacities
+milks = [m1,m2,m3]
+c = [c1,c2,c3]
 
-#he pours as much milk as possible until either bucket a becomes empty or bucket b becomes full.
-
-def pourAtoB(a,b):
-  if milks[0]+milks[1]<=c2:
-    milks[1]+=milks[0]
-    milks[0]= 0
+def pour(a,b,i):
+  
+  if milks[a] + milks[b] <= c[i]:
+    milks[b] += milks[a]
+    milks[a] = 0
   else:
-    change = milks[0]+milks[1]-c2
-    milks[1] = c2 
-    milks[0] = change
+    change = milks[b]-c[i]
+    milks[b] = c[i]
+    milks[a] += change
 
-def pourBtoC(b,c):
-  if milks[1]+milks[2]<=c3:
-    milks[2]+=milks[1]
-    milks[1]= 0
-  else:
-    change = milks[1]+milks[2]-c3
-    milks[2] = c3 
-    milks[1] = change
-    
-def pourCtoA(c,a):
-  if milks[2]+milks[0]<=c1:
-    milks[0]+=milks[2]
-    milks[2]= 0
-  else:
-    change = milks[2]+milks[0]-c1
-    milks[0] = c1
-    milks[2] = change 
+for _ in range(33):
+  pour(0,1,1)
+  pour(1,2,2)
+  pour(2,0,0)
+pour(0,1,1)
 
-for i in range(1,34):
-  pourAtoB(milks[0],milks[1])
-  pourBtoC(milks[1],milks[2])
-  pourCtoA(milks[2],milks[0])
-
-
-pourAtoB(milks[0],milks[1])
-
-for i in range(3):
-  print(milks[i])
+for i in milks:
+  print(i)
 
 
