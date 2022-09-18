5356170bf4d88796a5fd2bb31acb16fdd0d6f692
tsx358 <97471993+tsx358@users.noreply.github.com>
2022-09-17 21:01:06 -0400

    Update billboard.py

--- 2017-2018/2017 December/billboard.py
+++ 2017-2018/2017 December/billboard.py
@@ -2,32 +2,20 @@ import sys
 sys.stdin = open("billboard.in","r")
 sys.stdout = open("billboard.out","w")
 
-bill1x1,bill1y1,bill1x2,bill1y2 = map(int, input().split())
-bill2x1,bill2y1,bill2x2,bill2y2 = map(int, input().split())
-tx1,ty1,tx2,ty2 = map(int, input().split())
+class Rect:
+  def __init__(self):
+    self.x1,self.y1,self.x2,self.y2 = map(int, input().split())
+  def area(self):
+    return (self.x2-self.x1)*(self.y2-self.y1)
 
-
-def area(recx1,recy1,recx2,recy2):
-  return (recx2-recx1)*(recy2-recy1)
-
-def intersect(recX1,recY1,recX2,recY2, rec2X1,rec2Y1,rec2X2,rec2Y2): #intersecting area of 2 recs
-  height = min(recY2,rec2Y2)-max(recY1,rec2Y1)
-  length = min(recX2,rec2X2)-max(recX1,rec2X1)
-  if min(height,length)>0:
-    return height*length
-  else:
-    return 0
-
-#one rec and truck intersects. find rec area - intersection. do that for our 2 recs/billboards, then add the 2 visible parts.
-
-a1 = area(bill1x1,bill1y1,bill1x2,bill1y2)
-a2 = area(bill2x1,bill2y1,bill2x2,bill2y2)
-
-i1 = intersect(bill1x1,bill1y1,bill1x2,bill1y2,tx1,ty1,tx2,ty2)
-i2 = intersect(bill2x1,bill2y1,bill2x2,bill2y2,tx1,ty1,tx2,ty2)
+def intersect(a,b):
+  overlapX = max(0, min(a.x2, b.x2) - max(a.x1, b.x1))
+  overlapY = max(0, min(a.y2, b.y2) - max(a.y1, b.y1))
+  return overlapX*overlapY
   
-visible1 = a1-i1
-visible2 = a2-i2
+rects = []
+for _ in range(3):
+  rects.append(Rect())
 
-print(visible1+visible2)
+print(rects[0].area()+rects[1].area()-intersect(rects[0],rects[2])-intersect(rects[1],rects[2]))
 
