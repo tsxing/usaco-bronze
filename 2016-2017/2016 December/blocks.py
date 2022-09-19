83cc049b13629fa80c21b8fdc23e28ad3479dad6
tsx358 <97471993+tsx358@users.noreply.github.com>
2022-09-19 17:18:49 -0400

    Update blocks.py

--- 2016-2017/2016 December/blocks.py
+++ 2016-2017/2016 December/blocks.py
@@ -3,28 +3,23 @@ sys.stdin = open("blocks.in","r")
 sys.stdout = open("blocks.out","w")
 
 N = int(input())
-w1s, w2s, final = [0] * 26, [0] * 26, [0] * 26 #initialize
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a1 = [0]*26
+a2 = [0]*26
+ans = [0]*26
 
 for _ in range(N):
   w1,w2 = map(str,input().split())
-  
   for i in w1:
-    for l in range(26):
-      if alphabet[l] == i:
-        w1s[l]+=1
-    
+    a1[a.index(i)]+=1
   for i in w2:
-    for l in range(26):
-      if alphabet[l] == i:
-        w2s[l]+=1
-        
+    a2[a.index(i)]+=1
+    
   for i in range(26):
-    final[i] += max(w1s[i],w2s[i]) #update final
+    ans[i]+= max(a1[i],a2[i])
 
-  w1s, w2s = [0] * 26, [0] * 26  #reset counts for individual blocks
-    
-    
-for i in range(26):
-  print(final[i])
+  a1,a2 = [0]*26,[0]*26
+
+for i in ans:
+  print(i)
