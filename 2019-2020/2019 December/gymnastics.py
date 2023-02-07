9efaeab4d776862585999a9049434f222a8e336d
Tina X <97471993+tsx358@users.noreply.github.com>
2023-02-06 23:15:57 -0500

    Update gymnastics.py

--- 2019-2020/2019 December/gymnastics.py
+++ 2019-2020/2019 December/gymnastics.py
@@ -1,28 +1,26 @@
 import sys
-sys.stdin = open("gymnastics.in","r")
+sys.stdin=open("gymnastics.in","r")
 sys.stdout = open("gymnastics.out","w")
 
-K,N = map(int, input().split())
-sessions = []
-
+K, N = map(int, input().split())
+better = [ [0]*20 for i in range(20)]
+rankings=[[0]*10 for i in range(20)]
+cows = []
 ans = 0
-tot = 0
-for _ in range(K):
+
+for i in range(K):
   line = [int(x) for x in input().split()]
-  sessions.append(line)
+  rankings[i] = [x-1 for x in line]
+  
+for i in range(K): #row
+  for j in range(N): #column
+    for x in range(j+1,N): # "the next cow being compared"
+      better[rankings[i][j]][rankings[i][x]]+=1
 
 for i in range(N):
   for j in range(N):
-    if i==j: #if the ones we're comparing is the same
-      continue
-    for s in sessions:
-      if s.index(i+1)<s.index(j+1): #+1 because we use 0 indexing
-        break
-    else:
-      ans += 1
-
+    if better[i][j]==K:
+      ans+=1
 print(ans)
-
-#this solution compares elements' indexes with 3 nested for loops
     
     
