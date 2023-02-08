9fc667bae0013a8ec237a0d3adc348a96d6925a5
Tina X <97471993+tsx358@users.noreply.github.com>
2023-02-07 22:12:09 -0500

    Update triangles.py

--- 2019-2020/2020 February/triangles.py
+++ 2019-2020/2020 February/triangles.py
@@ -1,23 +1,19 @@
-from itertools import permutations
 import sys
-sys.stdin = open("triangles.in","r")
+sys.stdin=open("triangles.in","r")
 sys.stdout = open("triangles.out","w")
-
 N = int(input())
-points = []
+pts = []
+ans = 0
 for _ in range(N):
   x,y = map(int, input().split())
-  points.append([x,y])
+  pts.append([x,y])
 
-allPermutations = permutations(points,3) #generate all permutations with size 3
+for p1 in range(N):
+  for p2 in range(N):
+    for p3 in range(N):
+      if pts[p1][0]==pts[p2][0] and pts[p2][1]==pts[p3][1]:
+        area = abs(pts[p1][1]-pts[p2][1])*abs(pts[p2][0]-pts[p3][0])
+        ans = max(ans, area)
 
-area = 0
-for i in allPermutations:
-  if i[0][1] == i[1][1] and i[1][0] == i[2][0]: 
-    #first condition  checks if it's parallel to x-axis. They have equal y values. vice versa for y-axis.
-    area = max(area,abs(i[0][0]-i[1][0])*abs(i[1][1]-i[2][1]))
-
-print(area)
-
-#problem notes:
-#note how to generate permutations with itertools 
+print(ans)
+    
