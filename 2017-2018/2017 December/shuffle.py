10df08baae11deb35f1dd32e51503aac19b3d84c
Tina X <97471993+tsx358@users.noreply.github.com>
2023-02-08 21:56:10 -0500

    Update shuffle.py

--- 2017-2018/2017 December/shuffle.py
+++ 2017-2018/2017 December/shuffle.py
@@ -1,21 +1,20 @@
 import sys
-sys.stdin = open("shuffle.in","r")
+sys.stdin=open("shuffle.in","r")
 sys.stdout = open("shuffle.out","w")
-
-N = int(input())
+            
+n = int(input())
 a = [int(x) for x in input().split()]
-afterShuffles = [int(x) for x in input().split()]
-def reShuffle(arr):
-  newArr = [0]*N
-  for i in range(N):
+ids = [int(x) for x in input().split()]
+
+def solve(arr):
+  newArr = [0]*n
+  for i in range(0,n):
     newArr[i] = arr[a[i]-1]
   return newArr
+  
+for _ in range(3):
+  ids = solve(ids)
 
-for i in range(3):
-  reShuffle(afterShuffles)
-ans = reShuffle(reShuffle(reShuffle(afterShuffles)))
-
-for i in range(N):
-  print(ans[i])
-
+for i in ids:
+  print(i)
 
